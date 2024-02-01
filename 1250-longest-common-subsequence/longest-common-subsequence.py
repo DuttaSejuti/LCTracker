ba8eabb8588f2202d3e9fe7dc549a4b3446ad2cc
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-02-01 18:26:11 +0100

    Time: 651 ms (36.09%) | Memory: 43 MB (30.08%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -36,3 +36,21 @@ class Solution:
         result = recursive_f(0,0)
         return result
 
+        # dp = list()
+        # m = len(text1)
+        # n = len(text2)
+
+        # for i in range(m+1):
+        #     row = []
+        #     for j in range(n+1):
+        #         row.append(0)
+        #     dp.append(row)
+        
+        # for i in range(1, m+1):
+        #     for j in range(1, n+1):
+        #         if text1[i-1] == text2[j-1]:
+        #             dp[i][j] = dp[i-1][j-1] + 1
+        #         else:
+        #             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+        # return dp[m][n]
