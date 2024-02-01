9d6c2358038f5c8b9d5dacd924035aebf258580f
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-02-01 18:24:59 +0100

    Update longest-common-subsequence.py with comments

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -4,6 +4,7 @@ class Solution:
         n = len(text2) 
         dp = []
 
+        # form the 2D dp array with one extra index in row and column
         for i in range(m+1):
             row = []
             for j in range(n+1):
@@ -11,21 +12,27 @@ class Solution:
             dp.append(row)
 
         def recursive_f(s1, s2):
+            #base case, end of either of the strings
             if s1 == m or s2 == n:
                 return 0
-            if dp[s1][s2] != -1:
+
+            #checks if the computation is already done or not, if done then a value exist in the dp already,
+            # then just fetch the value from the dp array
+            if dp[s1][s2] != -1: 
                 return dp[s1][s2]
+
+            # if a match is found, then add +1, and run the function for the later characters
             if text1[s1] == text2[s2]:
                 res = 1 + recursive_f(s1+1, s2+1)
                 dp[s1][s2] = res
+
+            # otherwise find the max
             else:
                 res = max(recursive_f(s1+1,s2), recursive_f(s1,s2+1))
                 dp[s1][s2] = res
+            
             return res
         
         result = recursive_f(0,0)
-        # print(result)
-
-        # print(dp)
         return result
 
