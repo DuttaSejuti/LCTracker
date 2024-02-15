288cd311faaa7c08da1730ff075572f7e88cf567
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-02-15 13:43:11 +0100

    Time: 98 ms (31.87%) | Memory: 22.3 MB (17.31%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,15 +1,26 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = list()
-        new_dict = dict()
+        # TC: O(m*n*26) = O(m*n), SC: O()
+        new_dict = defaultdict(list) #default value is a list; charCountArray: [anaagrams]
 
-        for i in range(len(strs)):
-            temp = "".join(sorted(strs[i]))
-            if temp not in new_dict:
-                new_dict[temp] = [strs[i]]
-            else:
-                new_dict[temp].append(strs[i])
+        for word in strs:
+            count = [0]*26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            new_dict[tuple(count)].append(word) #list can not be keys,so tuple
+        return new_dict.values()
+
+        # TC: O(m*nlogn) SC: O(n)
+        # result = list()
+        # new_dict = dict()
+
+        # for i in range(len(strs)):
+        #     temp = "".join(sorted(strs[i]))
+        #     if temp not in new_dict:
+        #         new_dict[temp] = [strs[i]]
+        #     else:
+        #         new_dict[temp].append(strs[i])
         
-        for v in new_dict.values():
-            result.append(v)
-        return result
+        # for v in new_dict.values():
+        #     result.append(v)
+        # return result
