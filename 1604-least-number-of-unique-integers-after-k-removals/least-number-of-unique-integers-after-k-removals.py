fed0a30dc8df95912ca8dc2248b712fcc2713b72
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-02-16 13:01:05 +0100

    Time: 350 ms (60.81%) | Memory: 42.5 MB (6.64%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.py
@@ -6,15 +6,15 @@ class Solution:
         for n in arr:
             new_dict[n] = new_dict.get(n, 0) + 1
         
-        sorted_new_dict = dict(sorted(new_dict.items(), key = lambda x:x[1]))
+        new_dict = dict(sorted(new_dict.items(), key = lambda x:x[1]))
         
-        for key in sorted_new_dict:
-            if sorted_new_dict[key] <= k:
-                k = k - sorted_new_dict[key]
+        for key in new_dict:
+            if new_dict[key] <= k:
+                k = k - new_dict[key]
                 # del new_dict[key]
                 count += 1
             else:
-                k = k - sorted_new_dict[key]
+                k = k - new_dict[key]
                 if k < 0:
                     break
 
