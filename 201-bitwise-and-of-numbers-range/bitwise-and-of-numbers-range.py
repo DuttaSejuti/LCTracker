60d8664dbe99389f79d484853059b1b06e71acf8
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-02-22 11:57:18 +0100

    Time: 70 ms (13.36%) | Memory: 16.6 MB (80.67%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.py
@@ -1,21 +1,28 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        max_bits = max(left.bit_length(), right.bit_length())
-        left = bin(left)[2:].zfill(max_bits)
-        right = bin(right)[2:]
-        ans = ''
-
-        for i in range(max_bits):
-            if left[i] != right[i]:
+        ans = int()
+        for i in range(31, -1, -1):
+            if left&(1<<i) != right&(1<<i):
                 break
-            ans += left[i]
+            ans |= left&(1<<i)
+        return ans
 
-        if not ans:
-            return 0
-        else:
-            ans += '0' * (max_bits - len(ans))
+        # max_bits = max(left.bit_length(), right.bit_length())
+        # left = bin(left)[2:].zfill(max_bits)
+        # right = bin(right)[2:]
+        # ans = ''
 
-        return int(ans, 2)
+        # for i in range(max_bits):
+        #     if left[i] != right[i]:
+        #         break
+        #     ans += left[i]
+
+        # if not ans:
+        #     return 0
+        # else:
+        #     ans += '0' * (max_bits - len(ans))
+
+        # return int(ans, 2)
 
         # ans = left
         # for i in range(left+1, right+1, 1):
