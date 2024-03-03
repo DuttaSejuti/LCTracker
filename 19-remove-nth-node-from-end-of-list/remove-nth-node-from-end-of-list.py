27cb717307065f4fb7d79e427e50a789adf2cc01
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-03 16:36:55 +0100

    Time: 41 ms (27.33%) | Memory: 16.6 MB (61.49%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.py
@@ -7,8 +7,7 @@
 # IDEA
 # INPUT => head = [1,2,3,4,5], n = 2, OUTPUT => [1,2,3,5]
 # we measure the length of the linkedList, then compute the index we need to remove
-# then remove the node; we replace the removed node's(4) property with the next node's(5) property
-
+# then remove the node by pointing the prev node's next to the removed node's(4) next(5)
 class Solution:
     def getLength(self, head: Optional[ListNode]) -> int:
         count = 0
@@ -25,12 +24,7 @@ class Solution:
         while(curr):
             i += 1
             if i == idx:
-                next_node = curr.next
-                if next_node:
-                    curr.val = next_node.val
-                    curr.next = next_node.next
-                else:
-                    prev.next = None # if the node to be removed is the tail node
+                prev.next = curr.next
                 break
             prev = curr 
             curr = curr.next
@@ -38,13 +32,12 @@ class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         length = self.getLength(head)
 
-        # the lowest value of n can be 1, if the length of the linkedList is 1, there is only 1 node
-        # if we have to remove that node, the head becomes None
-        if length == 1:
-            head = None
-            return head
+        nodeToBeRemovedIndex = length - n + 1 # 1-index; idx from the start
+        
+        # if the node to be remove is the head itself, then head becomes None
+        if nodeToBeRemovedIndex == 1: 
+            return head.next
 
-        nodeToBeRemovedIndex = length - n + 1 # 1-index
         self.deleteNode(head, nodeToBeRemovedIndex)
         return head
         
