b742446cedcfcf4e1dc8858c7a744a249732b808
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-04 18:06:37 +0100

    Time: 49 ms (12.75%) | Memory: 18.4 MB (5.69%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.py
@@ -5,39 +5,65 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findHightestLevel(self, root: Optional[TreeNode]) -> int:
-        level = 0
+     def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
         queue = deque()
 
-        if root:
+        if not root:
+            return None
+        else:
             queue.append(root)
 
-        while(len(queue) > 0):
-            for i in range(len(queue)):
-                v = queue.popleft()
-                if v.left:
-                    queue.append(v.left)
-                if v.right:
-                    queue.append(v.right)
-            level += 1
+        # queue = deque([root])
+        leftmost_val = root.val
 
-        return level
+        while len(queue) > 0:
+            level_size = len(queue)
+
+            for i in range(level_size):
+                node = queue.popleft()
+
+                if i == 0:  # The first node at each level
+                    leftmost_val = node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+        return leftmost_val
+    # def findHightestLevel(self, root: Optional[TreeNode]) -> int:
+    #     level = 0
+    #     queue = deque()
+
+    #     if root:
+    #         queue.append(root)
+
+    #     while(len(queue) > 0):
+    #         for i in range(len(queue)):
+    #             v = queue.popleft()
+    #             if v.left:
+    #                 queue.append(v.left)
+    #             if v.right:
+    #                 queue.append(v.right)
+    #         level += 1
+
+    #     return level
         
-    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        queue = deque()
-        h = self.findHightestLevel(root)
-        level = 0
+    # def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+    #     queue = deque()
+    #     h = self.findHightestLevel(root)
+    #     level = 0
 
-        if root:
-            queue.append(root)
+    #     if root:
+    #         queue.append(root)
 
-        while(len(queue) > 0):
-            for i in range(len(queue)):
-                v = queue.popleft()
-                if level+1 == h:
-                    return v.val
-                if v.left:
-                    queue.append(v.left)
-                if v.right:
-                    queue.append(v.right)
-            level += 1
+    #     while(len(queue) > 0):
+    #         for i in range(len(queue)):
+    #             v = queue.popleft()
+    #             if level + 1 == h:
+    #                 return v.val
+    #             if v.left:
+    #                 queue.append(v.left)
+    #             if v.right:
+    #                 queue.append(v.right)
+    #         level += 1
