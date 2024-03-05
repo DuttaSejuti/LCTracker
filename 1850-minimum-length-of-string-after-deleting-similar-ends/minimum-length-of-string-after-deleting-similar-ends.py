3c17222af475c224b9a5e0e790ee376e06a5ccbc
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-05 17:32:44 +0100

    Time: 54 ms (98.58%) | Memory: 17.5 MB (53.55%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.py
@@ -2,13 +2,31 @@ class Solution:
     def minimumLength(self, s: str) -> int:
         l, r = 0, len(s) - 1
 
-        while l < r and s[l] == s[r]:
-            while l < r and s[l] == s[l+1]:
+        while l < r  and s[l] == s[r]:
+            c = s[l] # get the first char of the string
+
+            # delete consecutive c from the prefix
+            # <= is used when we have "cabaabac", at the end we have "aa"
+            while l <= r and s[l] == c:
                 l += 1
-            while l < r and s[r] == s[r-1]:
+
+            # delete consecutive c from the suffix
+            while l < r and s[r] == c:
                 r -= 1
 
-            s = s[l+1:r]
-            l, r = 0, len(s) - 1
+        return r-l+1
 
-        return len(s)
+
+        # Not good as we are modifing the string
+        # l, r = 0, len(s) - 1
+
+        # while l < r and s[l] == s[r]:
+        #     while l < r and s[l] == s[l+1]:
+        #         l += 1
+        #     while l < r and s[r] == s[r-1]:
+        #         r -= 1
+
+        #     s = s[l+1:r]
+        #     l, r = 0, len(s) - 1
+
+        # return len(s)
