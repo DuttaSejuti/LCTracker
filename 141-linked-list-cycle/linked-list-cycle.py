5776fd3e08c7f68ca25f29fa52b9570c1662b0c0
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-06 11:11:52 +0100

    Time: 53 ms (15.77%) | Memory: 19 MB (96.32%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,17 +6,34 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # keeping a slow and fast pointer, slow increases by 1 node, fast pointer increases by 2 node
+        # If the two pointers meets, then there's a cycle
+        # TC: O(n), SC:O(1)
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+        
+        return False
+
 
         # will keep a hashSet to keep track of the visited node, if the node is already in the set
         # there is a cycle; using hashSet instead of hashMap because there can be duplicate node val
-        hash_set = set()
-        curr = head
+        # in the list.
+        # TC:O(n); SC:O(n)
 
-        while(curr):
-            if curr not in hash_set:
-                hash_set.add(curr)
-            else:
-                return True
-            curr = curr.next
+        # hash_set = set()
+        # curr = head
 
-        return False
+        # while(curr):
+        #     if curr not in hash_set:
+        #         hash_set.add(curr)
+        #     else:
+        #         return True
+        #     curr = curr.next
+
+        # return False
