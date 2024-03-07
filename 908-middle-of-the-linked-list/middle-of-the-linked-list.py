cc1ca3fdccc0258bf097d3a999245e4058a2ce54
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-07 15:15:27 +0100

    Time: 38 ms (36.42%) | Memory: 16.6 MB (25.02%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -8,12 +8,35 @@
 # 1) get the length of the linked list, if even m = l // 2, else m = l//2 + 1; then traverse
 # the linked list with i = 1 and return head while i == m
 # 2) use slow-fast paced two pointer, traverse the linked list with slow, fast pointer, if !fast
+# or !fast.next, return slow. Simply, return slow when you get to the end of the linked list.
+
 class Solution:
+    def getLength(self, head: Optional[ListNode]) -> int:
+        curr = head
+        l = 0
+
+        while curr:
+            l += 1
+            curr = curr.next
+        return l
+    
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        slow, fast = head, head
+        l = self.getLength(head)
+        m = (l // 2) + 1
+        i = 1
+        curr = head
 
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
+        while curr:
+            if i == m:
+                return curr
+            curr = curr.next
+            i += 1
 
-        return slow
+        # TC: O(n), SC: O(1)
+        # slow, fast = head, head
+
+        # while fast and fast.next:
+        #     slow = slow.next
+        #     fast = fast.next.next
+
+        # return slow
