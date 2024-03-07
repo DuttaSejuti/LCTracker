8b0ea17d3c18f6fe8ab4918e4f92ecce084b698b
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-07 15:17:49 +0100

    Time: 31 ms (80.81%) | Memory: 16.6 MB (46.37%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -5,38 +5,41 @@
 #         self.next = next
 
 # IDEA
-# 1) get the length of the linked list, if even m = l // 2, else m = l//2 + 1; then traverse
-# the linked list with i = 1 and return head while i == m
+# 1) get the length of the linked list, m = l//2 + 1; then traverse
+# the linked list with i = 1 and return head while i == m 
+# ^^ not good as we are traversing the list twice
+
 # 2) use slow-fast paced two pointer, traverse the linked list with slow, fast pointer, if !fast
 # or !fast.next, return slow. Simply, return slow when you get to the end of the linked list.
 
 class Solution:
-    def getLength(self, head: Optional[ListNode]) -> int:
-        curr = head
-        l = 0
-
-        while curr:
-            l += 1
-            curr = curr.next
-        return l
-    
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        l = self.getLength(head)
-        m = (l // 2) + 1
-        i = 1
-        curr = head
+        # approach 2 TC: O(n), SC: O(1)
+        slow, fast = head, head
 
-        while curr:
-            if i == m:
-                return curr
-            curr = curr.next
-            i += 1
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
 
-        # TC: O(n), SC: O(1)
-        # slow, fast = head, head
+        return slow
 
-        # while fast and fast.next:
-        #     slow = slow.next
-        #     fast = fast.next.next
+    # def getLength(self, head: Optional[ListNode]) -> int:
+    #     curr = head
+    #     l = 0
 
-        # return slow
+    #     while curr:
+    #         l += 1
+    #         curr = curr.next
+    #     return l
+    
+    # def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     l = self.getLength(head)
+    #     m = (l // 2) + 1
+    #     i = 1
+    #     curr = head
+
+    #     while curr:
+    #         if i == m:
+    #             return curr
+    #         curr = curr.next
+    #         i += 1
