70901e4345f3dd25bc1d284af56c1ec85317d0e1
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-09 02:05:46 +0100

    Time: 354 ms (47.91%) | Memory: 35.5 MB (73.75%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.py
+++ 2634-minimum-common-value/minimum-common-value.py
@@ -1,10 +1,40 @@
+#IDEA
+# get the nums1 array in a dictionary, for iterating over num2, check if new_dict.get[n2], return n2
+# as the numbers are sorted, if we get a common element, no need to find the rest common
+# we only need to return the lowest common number
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        new_dict = dict()
-        for n1 in nums1:
-            new_dict[n1] = new_dict.get(n1, 0) + 1
+        # with Two pointers
+        # TC: O(n+m), SC: O(1)
+        first, second = 0, 0
+        l1, l2 = len(nums1), len(nums2)
 
-        for n2 in nums2:
-            if new_dict.get(n2):
-                return n2
+        while first < l1 and second < l2:
+            if nums1[first] == nums2[second]:
+                return nums1[first]
+            elif nums1[first] < nums2[second]:
+                first += 1
+            else: # nums1[first] > nums2[second]
+                second += 1
         return -1
+
+        # with Set intersection
+        # set1 = set(nums1)
+        # set2 = set(nums2)
+        # common = set1.intersection(set2)
+
+        # if common:
+        #     return min(common)
+        # else:
+        #     return -1
+
+        # With HashMap
+        # TC: O(n+m), SC:O(n)
+        # new_dict = dict()
+        # for n1 in nums1:
+        #     new_dict[n1] = new_dict.get(n1, 0) + 1
+
+        # for n2 in nums2:
+        #     if new_dict.get(n2):
+        #         return n2
+        # return -1
