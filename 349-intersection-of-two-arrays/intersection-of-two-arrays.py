528793c62c1ebec6a9b900e3c76c97dc5fa8ddee
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-10 21:11:36 +0100

    Time: 46 ms (69.23%) | Memory: 16.7 MB (91.17%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,9 +1,25 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        set1 = set(nums1)
-        set2 = set(nums2)
+        # TC: O(n+m), SC:O(n)
+        new_dict = dict()
+        res = list()
 
-        common = set1.intersection(set2)
+        for n1 in nums1:
+            new_dict[n1] = 1
+        
+        for n2 in nums2:
+            # check if n2 is in n1/dict and n2 already in result array;
+            if n2 in new_dict and new_dict[n2] == 1:
+                res.append(n2)
+                new_dict[n2] = 0 # so that duplicate number can't append in res array
 
-        return list(common)
+        return res
+
+        # # TC: O(n+m), SC: O(n+m)
+        # set1 = set(nums1)
+        # set2 = set(nums2)
+
+        # common = set1.intersection(set2)
+
+        # return list(common)
         
