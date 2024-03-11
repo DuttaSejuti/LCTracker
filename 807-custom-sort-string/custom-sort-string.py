01758a24db98020605fc9645a54891ec52338429
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-11 12:40:56 +0100

    Time: 34 ms (68.03%) | Memory: 16.6 MB (61.09%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.py
+++ 807-custom-sort-string/custom-sort-string.py
@@ -1,5 +1,6 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
+        # TC:O(n), SC:O(n)
         freq_s = dict()
         result = ''
 
@@ -10,16 +11,39 @@ class Solution:
         
         # iterating the order string, as we need to maintain the order of this string
         for char in order:
-            if char in freq_s and freq_s.get(char):
-                # if a char exists in order, the #of times it occur in the 
-                # string s has to maintain order; char * freq
-                result += char * freq_s.get(char)
-                freq_s[char] = 0
+            while char in freq_s and freq_s[char] > 0:
+                freq_s[char] -= 1
+                result += char
 
         # to append the rest of the char of s in the result string
         for f in freq_s:
-            if freq_s[f] > 0:
-                result += freq_s[f] * f # as the order doesn't matter anymore
+            while f in freq_s and freq_s[f] > 0:
+                freq_s[f] -= 1
+                result += f
         
         return result
+        
+        # # TC:O(n), SC:O(n)
+        # freq_s = dict()
+        # result = ''
+
+        # # keeping the freq of each char in s; s can have duplicate char
+        # # which would append in the result string; order will have unique char only
+        # for char in s:
+        #     freq_s[char] = freq_s.get(char, 0) + 1
+        
+        # # iterating the order string, as we need to maintain the order of this string
+        # for char in order:
+        #     if char in freq_s and freq_s.get(char):
+        #         # if a char exists in order, the #of times it occur in the 
+        #         # string s has to maintain order
+        #         result += char * freq_s.get(char)
+        #         freq_s[char] = 0
+
+        # # to append the rest of the char of s in the result string
+        # for f in freq_s:
+        #     if freq_s[f] > 0:
+        #         result += freq_s[f] * f # as the order doesn't matter anymore
+        
+        # return result
         
