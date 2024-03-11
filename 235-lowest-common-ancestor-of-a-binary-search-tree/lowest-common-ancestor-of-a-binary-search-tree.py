2911a6208891a5974b7763da953d746aa37cb4b1
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-11 13:23:04 +0100

    Time: 64 ms (12.15%) | Memory: 20.3 MB (54.89%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -14,21 +14,39 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not root:
-            return
+        # recursive solution
+        # TC: O(logn) => we are visiting one node for every single level of the tree, SC: O(logn) => recursive call stack
+        # if not root:
+        #     return
         
-        # Case 1
-        if root.val > p.val and root.val > q.val:
-            return self.lowestCommonAncestor(root.left, p, q)
+        # # Case 1
+        # if root.val > p.val and root.val > q.val:
+        #     return self.lowestCommonAncestor(root.left, p, q)
 
-        # Case 2
-        if root.val < p.val and root.val < q.val:
-            return self.lowestCommonAncestor(root.right, p, q)
+        # # Case 2
+        # if root.val < p.val and root.val < q.val:
+        #     return self.lowestCommonAncestor(root.right, p, q)
 
-        # Case 3
-        if (root.val > p.val and root.val < q.val) or (root.val < p.val and root.val > q.val):
-            return root
+        # # Case 3
+        # if (root.val > p.val and root.val < q.val) or (root.val < p.val and root.val > q.val):
+        #     return root
 
-        # Case 4
-        if root.val == p.val or root.val == q.val:
-            return root
+        # # Case 4
+        # if root.val == p.val or root.val == q.val:
+        #     return root
+
+
+        # iterative solution
+        # TC: O(logn), SC: O(1)
+        curr = root
+
+        if not curr:
+            return
+
+        while curr:
+            if curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            elif curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            else:
+                return curr
