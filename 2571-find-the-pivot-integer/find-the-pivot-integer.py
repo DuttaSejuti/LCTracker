f85adae3b9aee188ae30a65492c6118dbcfb4dfd
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-13 12:17:31 +0100

    Time: 51 ms (45.44%) | Memory: 16.6 MB (59.17%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,20 +1,33 @@
 # IDEA
 # approach 1) construst the array from 1 to n, take two sum, one from 1 to i, another 1 to n,
             # if they are equal we have found the pivot
+# approach 2) construct prefix sum list, looping from 1 to n, 
 
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        prefix_sum = [0]
-        
-        for i in range(1, n+1):
-            prefix_sum.append(prefix_sum[i-1]+i)
-
-        last_sum = prefix_sum[-1]
+        # TC: O(n), SC: O(1)
+        sum_from_1 = 0
+        total_sum = n*(n+1) // 2
 
         for i in range(1, n+1):
-            if prefix_sum[i] == (last_sum - prefix_sum[i-1]):
+            sum_from_1 += i
+            if sum_from_1 == total_sum - (sum_from_1 - i):
                 return i
         return -1
+
+        # TC: O(n), SC: O(n)
+        # prefix_sum = [0]
+        
+        # # constructing the prefix_sum
+        # for i in range(1, n+1):
+        #     prefix_sum.append(prefix_sum[i-1]+i)
+
+        # last_sum = prefix_sum[-1] #similar as n(n+1)//2
+
+        # for i in range(1, n+1):
+        #     if prefix_sum[i] == (last_sum - prefix_sum[i-1]):
+        #         return i
+        # return -1
 
         # TC: O(n^2), SC: O(n)
         # nums = list()
