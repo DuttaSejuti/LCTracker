64ed92e33eabe0a6d9e4a5dbc2a2dce221642baa
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-13 11:57:10 +0100

    Time: 57 ms (35.75%) | Memory: 16.5 MB (92.38%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -4,19 +4,32 @@
 
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        nums = list()
-        sum_from_1 = 0 # calculating sum from 1 to i
-        sum_from_i = 0 # calculating sum from i to n
-
-        # construct the list in range 1 to n inclusive
+        prefix_sum = [0]
+        
         for i in range(1, n+1):
-            nums.append(i)
+            prefix_sum.append(prefix_sum[i-1]+i)
 
-        for i in range(len(nums)):
-            sum_from_1 += nums[i]
-            for j in range(i, len(nums)):
-                sum_from_i += nums[j]
-            if sum_from_1 == sum_from_i:
-                return nums[i]
-            sum_from_i = 0 # as we need to calculate the sum once again
+        last_sum = prefix_sum[-1]
+
+        for i in range(1, n+1):
+            if prefix_sum[i] == (last_sum - prefix_sum[i-1]):
+                return i
         return -1
+
+        # TC: O(n^2), SC: O(n)
+        # nums = list()
+        # sum_from_1 = 0 # calculating sum from 1 to i
+        # sum_from_i = 0 # calculating sum from i to n
+
+        # # construct the list in range 1 to n inclusive
+        # for i in range(1, n+1):
+        #     nums.append(i)
+
+        # for i in range(len(nums)):
+        #     sum_from_1 += nums[i]
+        #     for j in range(i, len(nums)):
+        #         sum_from_i += nums[j]
+        #     if sum_from_1 == sum_from_i:
+        #         return nums[i]
+        #     sum_from_i = 0 # as we need to calculate the sum once again
+        # return -1
