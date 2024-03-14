25795cbc2cb88a044d608368fb679a406eaf3e3e
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-14 16:21:16 +0100

    Time: 225 ms (73.09%) | Memory: 19.5 MB (77.01%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,23 +1,22 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         # TC: O(n), SC: O(n)
-        freq = dict() # prefix_sum: # of occurances of this prefix_sum
-        freq[0] = 1 # to resolve the case where we get prefix_sum = 0 in future
+        freq = dict() # prefix_sum: # of occurances of this currSum
+        freq[0] = 1 # to resolve the case where we get currSum = 0 in future
         result = 0
-        prefix_sum = 0
+        currSum = 0
 
         for i in range(len(nums)):
-            prefix_sum += nums[i]
+            currSum += nums[i]
 
-            # temp makes sure the possible no of subarray in the middle
-            temp = prefix_sum - k
-            if temp in freq:
-                result += freq[temp]
+            # if we chop of a prefix of diff from the subarray we will get a subarray of k
+            diff = currSum - k
 
-            # if the temp not in freq, insert the prefix_sum in map
-            # even if temp in freq, we need to increase the frequency of the prefix_sum
-            # as we are encounted this prefix_sum in the past 
-            freq[prefix_sum] = freq.get(prefix_sum, 0) + 1
+            # this is basically checking if the diff in the map ot not, if present get the freq
+            result += freq.get(diff, 0)
+
+            # we need to check if we have encountered the diff previously, the prefix_sum that we need to chop off from the subarray
+            freq[currSum] = freq.get(currSum, 0) + 1
 
         return result
 
