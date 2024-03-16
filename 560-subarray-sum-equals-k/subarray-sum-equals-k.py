4c777524fdb8fabb8067a600b3534b55ea5d0e02
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-16 17:57:49 +0100

    Time: 257 ms (5.39%) | Memory: 20.4 MB (20.57%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,24 +1,48 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # TC: O(n), SC: O(n)
-        freq = dict() # prefix_sum: # of occurances of this currSum
-        freq[0] = 1 # to resolve the case where we get currSum = 0 in future
-        result = 0
-        currSum = 0
-
+        count = 0
+        ps = 0
+        prefix_sum = dict()
+        prefix_sum_count = dict()
+        # prefix_sum_count[0] = 1
         for i in range(len(nums)):
-            currSum += nums[i]
+            ps += nums[i]
+            prefix_sum[i] = ps
+            if ps == k:
+                count += 1
 
-            # if we chop of a prefix of diff from the subarray we will get a subarray of k
-            diff = currSum - k
+            known_variable = (prefix_sum[i] - k) 
+            
+            count += prefix_sum_count.get(known_variable, 0)
 
-            # this is basically checking if the diff in the map ot not, if present get the freq
-            result += freq.get(diff, 0)
+            prefix_sum_count[ps] = prefix_sum_count.get(ps, 0) + 1
 
-            # we need to check if we have encountered the diff previously, the prefix_sum that we need to chop off from the subarray
-            freq[currSum] = freq.get(currSum, 0) + 1
+            # for j in range(0, i):
+            #     condition = (prefix_sum[j] == known_variable)
+            #     # condition = (prefix_sum[i] - prefix_sum[j] == k)
+            #     count += condition
+        return count
 
-        return result
+
+        # TC: O(n), SC: O(n)
+        # freq = dict() # prefix_sum: # of occurances of this currSum
+        # freq[0] = 1 # to resolve the case where we get currSum = 0 in future
+        # result = 0
+        # currSum = 0
+
+        # for i in range(len(nums)):
+        #     currSum += nums[i]
+
+        #     # if we chop of a prefix of diff from the subarray we will get a subarray of k
+        #     diff = currSum - k
+
+        #     # this is basically checking if the diff in the map ot not, if present get the freq
+        #     result += freq.get(diff, 0)
+
+        #     # we need to check if we have encountered the diff previously, the prefix_sum that we need to chop off from the subarray
+        #     freq[currSum] = freq.get(currSum, 0) + 1
+
+        # return result
 
         # TC: O(n^2), SC: O(1) => TLE
         # count = 0
