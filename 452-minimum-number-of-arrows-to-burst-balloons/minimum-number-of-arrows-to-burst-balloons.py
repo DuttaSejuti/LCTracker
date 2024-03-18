59d3b775d1f9588dc4489c773c4c06c3298c53cf
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-18 19:36:23 +0100

    Time: 1039 ms (65.10%) | Memory: 62.8 MB (31.08%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -1,52 +1,21 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points = sorted(points, key = lambda x:x[0])
-        # points = sorted(new_points, key = lambda x:x[1])
-        nonOverlappedPoints = list()
-        i = 0
-        print(points)
+        if not points:
+            return 0
 
-        if len(points) == 1:
-            return 1
+        # Sort intervals based on the end points
+        points.sort(key=lambda x: x[1])
 
-        while i < len(points)-1:
-            currPoint = points[i]
-            nextPoint = points[i+1]
+        nonOverlappedPoints = []
+        end = points[0][1]
 
-            # non-overlap points condition
-            if currPoint[1] < nextPoint[0]:
-                nonOverlappedPoints.append(currPoint)
-                
-                if i+1 == len(points) - 1:
-                    nonOverlappedPoints.append(nextPoint)
-                    break
-            else:
-                newPoint = [max(currPoint[0], nextPoint[0]), min(currPoint[1], nextPoint[1])]
-                
-                if i + 1 == len(points) - 1:
-                    nonOverlappedPoints.append(newPoint)
-                    break
-                
-                i += 2
-                while i < len(points):
-                    thirdPoint = points[i]
-                    if newPoint[1] < thirdPoint[0]:
-                        nonOverlappedPoints.append(newPoint)
+        for i in range(1, len(points)):
+            nextPoint = points[i]
+            start = nextPoint[0]
+            if start > end:
+                nonOverlappedPoints.append(end)
+                end = points[i][1]
 
-                        if i == len(points) - 1:
-                            nonOverlappedPoints.append(thirdPoint)
-
-                        i -= 1
-                        break
-                    if newPoint[1] >= thirdPoint[0]:
-                        newPoint = [max(newPoint[0], thirdPoint[0]), min(newPoint[1], thirdPoint[1])]
-
-                        if i == len(points) - 1:
-                            nonOverlappedPoints.append(newPoint)
-                    i += 1
-
-            i += 1
+        nonOverlappedPoints.append(end)
 
         return len(nonOverlappedPoints)
-
-
