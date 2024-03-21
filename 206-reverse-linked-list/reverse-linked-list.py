06790a46d06254764d2913163c50484663505133
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-21 14:34:12 +0100

    Time: 43 ms (18.71%) | Memory: 17.7 MB (77.25%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.py
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -10,30 +10,30 @@
 #  prev  curr next
 #        prev curr next
 class Solution:
-    # def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-    #     # TC: O(n), SC:O(1)
-    #     prev = None
-    #     curr = head
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # TC: O(n), SC:O(1)
+        prev = None
+        curr = head
         
-    #     while curr:
-    #         next_node = curr.next # store the next pointer
-    #         curr.next = prev # curr.next will be pointing to the previous node
-    #         prev = curr # curr will be the new prev
-    #         curr = next_node # we need to traverse the list
+        while curr:
+            next_node = curr.next # store the next pointer
+            curr.next = prev # curr.next will be pointing to the previous node
+            prev = curr # curr will be the new prev
+            curr = next_node # we need to traverse the list
 
-    #     return prev
+        return prev
 
         # Recursion solution
         # TC: O(n), SC: O(n)
-        def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
             
-            def reverse(curr, prev):
-                if not curr:
-                    return prev
+        #     def reverse(curr, prev):
+        #         if not curr:
+        #             return prev
 
-                next_node = curr.next
-                curr.next = prev
+        #         next_node = curr.next
+        #         curr.next = prev
                 
-                return reverse(next_node, curr) #basically it's doing curr = next_node, prev = curr
+        #         return reverse(next_node, curr) #basically it's doing curr = next_node, prev = curr
 
-            return reverse(head, None) # initial call curr = head, prev = None
+        #     return reverse(head, None) # initial call curr = head, prev = None
