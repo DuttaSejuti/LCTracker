27adad6cf72be53d8a413b3352820dcbe7afc4f0
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-29 17:53:50 +0100

    Time: 884 ms (53.82%) | Memory: 31.4 MB (26.72%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -1,17 +1,21 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
+        # TC:O(n), SC:O(1)
         max_num = max(nums)
         l, r = 0, 0
         result = 0
-        new_dict = dict()
+        count = 0
         n = len(nums)
 
         for r in range(len(nums)):
             if nums[r] == max_num:
-                new_dict[max_num] = new_dict.get(max_num, 0) + 1
-            while l <= r and new_dict.get(max_num, 0) == k:
+                count += 1
+            while l <= r and count == k:
                 if nums[l] == max_num:
-                    new_dict[max_num] -= 1
+                    count -= 1
+
+                # this makes sure that the window itself a valid subarray
+                # plus all the subarrays with the rest of the right elemets are also valid
                 result += n - r
                 l += 1
         return result
