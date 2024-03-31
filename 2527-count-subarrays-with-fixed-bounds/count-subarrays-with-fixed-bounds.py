beab354c4d487fcd1590627456c4944fc431e96d
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-03-31 19:35:37 +0200

    Time: 886 ms (9.64%) | Memory: 31.2 MB (8.54%) - LeetSync

---
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.py
@@ -0,0 +1,86 @@
+class Solution:
+    def countSubarraysWhereTargetExistsAtLeastKTimes(self, nums: List[int], k: int, target: int) -> int:
+        # TC:O(n), SC:O(1)
+        l, r = 0, 0
+        result = 0
+        count = 0
+        n = len(nums)
+
+        for r in range(len(nums)):
+            if nums[r] == target:
+                count += 1
+            while l <= r and count == k:
+                if nums[l] == target:
+                    count -= 1
+
+                # this makes sure that the window itself a valid subarray
+                # plus all the subarrays with the rest of the right elemets are also valid
+                result += n - r
+                l += 1
+
+            # similarly this will work instead of result += n-r
+            # result += l
+        return result
+
+    def breakdownList(self, nums: List[int], minK: int, maxK: int) -> List[List[int]]:
+        new_list = []
+
+        l, r = 0, 0
+
+        while l < len(nums):
+            while r < len(nums) and minK <= nums[r] <= maxK:
+                r += 1
+            # no valid subarray
+            if l == r:
+                r += 1
+            else:
+                new_list.append(nums[l:r])
+            l = r
+        return new_list
+
+    def countSubarraysWhereMaxExists(self, valid_list: List[int], k: int, boundary: int) -> int:
+        valid_lists = self.breakdownList(valid_list, boundary + 1, k)
+
+        count = 0
+        for valid_list in valid_lists:
+            count += self.countSubarraysWhereTargetExistsAtLeastKTimes(valid_list, 1, k)
+        return count
+    
+    def countSubarraysWhereMinExists(self, valid_list: List[int], k: int, boundary: int) -> int:
+        valid_lists = self.breakdownList(valid_list, k, boundary - 1)
+
+        count = 0
+        for valid_list in valid_lists:
+            count += self.countSubarraysWhereTargetExistsAtLeastKTimes(valid_list, 1, k)
+        return count
+    
+    def countSubarrayWhereNoneExists(self, valid_list: List[int], minK: int, maxK: int) -> int:
+        valid_lists = self.breakdownList(valid_list, minK + 1, maxK - 1)
+
+        count = 0
+        for valid_list in valid_lists:
+            valid_list_length = len(valid_list)
+            count += (valid_list_length * (valid_list_length + 1)) // 2
+
+        return count
+
+    def countValidSubarrays(self, valid_list: List[int], minK: int, maxK: int) -> int:
+        length = len(valid_list)
+        total_subarray = (length * (length + 1)) // 2
+
+        max_exists = self.countSubarraysWhereMaxExists(valid_list, maxK, minK)
+        min_exists = self.countSubarraysWhereMinExists(valid_list, minK, maxK)
+        none_exists = self.countSubarrayWhereNoneExists(valid_list, minK, maxK)
+
+        # print(max_exists, min_exists, none_exists)
+
+        return total_subarray - (max_exists + min_exists + none_exists)
+
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        valid_lists = self.breakdownList(nums, minK, maxK)
+
+        count = 0
+        for valid_list in valid_lists:
+            count += self.countValidSubarrays(valid_list, minK, maxK)
+
+        return count
