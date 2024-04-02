f940ba9e97bdd6c96e5ef3c920d35ab39f723771
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-02 13:14:06 +0200

    Time: 35 ms (91.42%) | Memory: 16.7 MB (64.59%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.py
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -1,42 +1,40 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        s_dict = dict()
-        t_dict = dict()
+        new_dict = dict()
 
-        converted_s = ''
-        converted_t = ''
+        if len(set(s)) != len(set(t)): # no of unique characters should be same 
+            return False
 
-        for i in s:
-            if i not in s_dict:
-                # mapping each character to a distinct number
-                s_dict[i] = len(s_dict) + 1
+        for i in range(len(s)):
+            if s[i] not in new_dict:
+                new_dict[s[i]] = t[i]
+            else:
+                if new_dict[s[i]] != t[i]:
+                    return False
+        
+        return True
 
-            converted_s += str(s_dict[i])
-        
-        for j in t:
-            if j not in t_dict:
-                t_dict[j] = len(t_dict) + 1
+    # def isIsomorphic(self, s: str, t: str) -> bool:
+    #     s_dict = dict()
+    #     t_dict = dict()
 
-            converted_t += str(t_dict[j])
-        
-        if converted_s == converted_t:
-            return True
-        
-        return False
+    #     converted_s = ''
+    #     converted_t = ''
 
-        # s_dict = dict()
-        # t_dict = dict()
-        
-        # for i in s:
-        #     s_dict[i] = s_dict.get(i, 0) + 1
-        
-        # for j in t:
-        #     t_dict[j] = t_dict.get(j, 0) + 1
-        
-        # if len(s_dict) != len(t_dict):
-        #     return False
-        
-        # if list(s_dict.values()) != list(t_dict.values()):
-        #     return False
+    #     for i in s:
+    #         if i not in s_dict:
+    #             # mapping each character to a distinct number
+    #             s_dict[i] = len(s_dict) + 1
 
-        # return True
+    #         converted_s += str(s_dict[i])
+        
+    #     for j in t:
+    #         if j not in t_dict:
+    #             t_dict[j] = len(t_dict) + 1
+
+    #         converted_t += str(t_dict[j])
+        
+    #     if converted_s == converted_t:
+    #         return True
+        
+    #     return False
