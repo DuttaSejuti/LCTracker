a55dbfb0010967840a5865f71890a2292970d386
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-04 14:52:29 +0200

    Time: 30 ms (86.71%) | Memory: 16.6 MB (38.30%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.py
@@ -1,29 +1,29 @@
 class Solution:
     # TC: O(n), SC:O(1)
-    # def maxDepth(self, s: str) -> int:
-    #     count = 0
-    #     max_depth = 0
-
-    #     for i in range(len(s)):
-    #         if s[i] == '(':
-    #             count += 1
-    #             max_depth = max(max_depth, count) # count will only increase here
-    #         if s[i] == ')':
-    #             count -= 1
-
-    #         # max_depth = max(max_depth, count)
-    #     return max_depth
-
-    # TC: O(n), SC:O(n)
     def maxDepth(self, s: str) -> int:
-        ans = 0
-        stack = list()
+        count = 0
+        max_depth = 0
 
         for i in range(len(s)):
             if s[i] == '(':
-                stack.append(s[i])
-                ans = max(ans, len(stack))
+                count += 1
+                max_depth = max(max_depth, count) # count will only increase here
             if s[i] == ')':
-                stack.pop()
+                count -= 1
 
-        return ans
+            # max_depth = max(max_depth, count)
+        return max_depth
+
+    # TC: O(n), SC:O(n)
+    # def maxDepth(self, s: str) -> int:
+    #     ans = 0
+    #     stack = list()
+
+    #     for i in range(len(s)):
+    #         if s[i] == '(':
+    #             stack.append(s[i])
+    #             ans = max(ans, len(stack))
+    #         if s[i] == ')':
+    #             stack.pop()
+
+    #     return ans
