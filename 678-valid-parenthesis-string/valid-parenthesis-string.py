6b16caed5e4640badd8c13ccae6ffa0049b3afb0
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-07 13:51:41 +0200

    Time: 53 ms (5.05%) | Memory: 17.3 MB (7.80%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -4,16 +4,13 @@ class Solution:
         pos, count = 0, 0
 
         def checkValid(pos, count) -> bool:
-            if count < 0:
-                return False 
+            if count < 0: # if count is negative, no need to go ahead, already invalid
+                return False
 
-            if pos == len(s):
-                if count == 0:
-                    return True
-                else:
-                    return False
+            if pos == len(s): # base: end of the string
+                return count == 0
             
-            if dp.get((pos, count)) != None:
+            if dp.get((pos, count)) != None: # dp[(pos, count)] exists
                 return dp[(pos, count)]
             
             if s[pos] == '(':
@@ -21,9 +18,9 @@ class Solution:
             if s[pos] == ')':
                 res = checkValid(pos+1, count-1)
             if s[pos] == '*':
-                res = (checkValid(pos+1, count+1)
-                or checkValid(pos+1, count-1)
-                or checkValid(pos+1, count))
+                res = (checkValid(pos+1, count+1) # '('
+                or checkValid(pos+1, count-1) # ')'
+                or checkValid(pos+1, count)) # ''
 
             dp[(pos, count)] = res
 
