9560576150d52d6cf4f0d005a4a7ef80e1ff0067
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-08 15:38:21 +0200

    Time: 40 ms (56.46%) | Memory: 16.6 MB (16.84%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,28 +1,16 @@
 class Solution:
+    # TC: O(n), SC: O(1)
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        count = 0
-        n = len(students)
-        new_dict = dict()
-
-
-        # keeping count of how many student want sandwich 0 and 1
+        count = [0, 0]  # count[0] for students who want sandwich 0, count[1] for students who want sandwich 1
+        
+        # Count the number of students who want each type of sandwich
         for student in students:
-            new_dict[student] = new_dict.get(student, 0) + 1
-        
-        students = deque(students)
-
-        while True:
-            if not sandwiches or new_dict.get(sandwiches[0], 0) == 0:
+            count[student] += 1
+            
+        for sandwich in sandwiches:
+            if count[sandwich] == 0:
                 break
-
-            top_sandwich = sandwiches[0]
-            student_choice = students.popleft()
-
-            # if student's choice matches with the stack top
-            if student_choice == top_sandwich:
-                sandwiches.pop(0)
-                new_dict[student_choice] -= 1
-            else:
-                students.append(student_choice) # pushing the popped student at the end
+            
+            count[sandwich] -= 1
         
-        return len(sandwiches)
+        return sum(count)
