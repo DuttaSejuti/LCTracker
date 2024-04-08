3499b90c213060250c7c937e56fc8fb8230f994b
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-08 16:54:22 +0200

    Time: 38 ms (72.82%) | Memory: 16.7 MB (16.84%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,16 +1,44 @@
 class Solution:
-    # TC: O(n), SC: O(1)
+    # TC:O(n), SC:O(1)
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
-        count = [0, 0]  # count[0] for students who want sandwich 0, count[1] for students who want sandwich 1
-        
-        # Count the number of students who want each type of sandwich
-        for student in students:
-            count[student] += 1
-            
+        res = len(students)
+        count = Counter(students) # return a hashMap
+
+        # the order of students does not matter
+        # the order of the sandwiches matters
         for sandwich in sandwiches:
-            if count[sandwich] == 0:
-                break
-            
-            count[sandwich] -= 1
+            if count[sandwich] > 0:
+                res -= 1
+                count[sandwich] -= 1
+            else:
+                return res
+        return res
+
+    # TC: O(n), SC:O(n) => SC O(n) because of the deque
+    # def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+    #     new_dict = dict()
+
+
+    #     # keeping count of how many student want sandwich 0 and 1
+    #     for student in students:
+    #         new_dict[student] = new_dict.get(student, 0) + 1
         
-        return sum(count)
+    #     students = deque(students)
+
+    #     while True:
+    #         # all the sandwiches are chosen sandwiches becomes empty OR
+    #         # the top sandwich is not wanted by any od the students => nothing to distribute 
+    #         if not sandwiches or new_dict.get(sandwiches[0], 0) == 0:
+    #             break
+
+    #         top_sandwich = sandwiches[0]
+    #         student_choice = students.popleft()
+
+    #         # if student's choice matches with the stack top
+    #         if student_choice == top_sandwich:
+    #             sandwiches.pop(0)
+    #             new_dict[student_choice] -= 1
+    #         else:
+    #             students.append(student_choice) # pushing the popped student at the end
+        
+    #     return len(sandwiches)
