1a9f1fd1d91e96e05bcd0d904a67f137afcb6064
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-09 12:56:14 +0200

    Time: 38 ms (85.49%) | Memory: 16.6 MB (51.83%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,14 +1,41 @@
 class Solution:
     # TC:O(n*m)=> n is the len of the tickets, m is the maximum no of iteration of outer while loop, SC:O(1)
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        sold_tickets = 0
+        sold_tickets = 0 # time taken
 
         # keep track of the sold_tickets in each pass untill the tickets[k] == 0
-        while True:
+        # while True:
+        #     for i in range(len(tickets)):
+        #         tickets[i] -= 1
+        #         if tickets[k] == 0:
+        #             sold_tickets += 1
+        #             return sold_tickets
+        #         if tickets[i] >= 0:
+        #             sold_tickets += 1
+
+        # another version of the while loop
+        while tickets[k] > 0:
             for i in range(len(tickets)):
-                tickets[i] -= 1
+                # buy a ticket at idx i if possible; tickets[i] > 0
+                if tickets[i] != 0:
+                    tickets[i] -= 1
+                    sold_tickets += 1
+                # if k bought all it's required tickets, return 
                 if tickets[k] == 0:
-                    sold_tickets += 1
                     return sold_tickets
-                if tickets[i] >= 0:
-                    sold_tickets += 1
+
+        return sold_tickets
+
+    # TC:O(n), SC:O(1)
+    # def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+    #     time = 0
+
+    #     for i in range(len(tickets)):
+    #         # current person is after k
+    #         if i > k:
+    #             time += min(tickets[k] - 1, tickets[i])
+    #         else:
+    #             # current person is k or current person is before k
+    #             time += min(tickets[k], tickets[i])
+            
+    #     return time
