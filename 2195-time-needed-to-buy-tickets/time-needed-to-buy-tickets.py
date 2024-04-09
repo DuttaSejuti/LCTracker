73a64f5fcc0ed609d3afb676ccfe214227089da4
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-09 13:07:08 +0200

    Time: 55 ms (18.16%) | Memory: 16.6 MB (51.83%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,7 +1,35 @@
 class Solution:
-    # TC:O(n*m)=> n is the len of the tickets, m is the maximum no of iteration of outer while loop, SC:O(1)
+    #Approach-01
+    # TC:O(n*m)=> m is the max no of tickets at each index, SC:O(n) => for the deque
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        sold_tickets = 0 # time taken
+        time = 0
+        queue = deque()
+
+        #initialize the queue with indices of the tickets
+        for i in range(len(tickets)):
+            queue.append(i)
+        
+        while len(queue) > 0:
+            time += 1 #increment time for each iteration
+
+            front = queue.popleft() # get the front of the queue
+            tickets[front] -= 1 # buy ticket for the front person
+
+            # if k has bought all the tickets, return
+            if front == k and tickets[front] == 0:
+                return time
+            
+            # push the front person at the end of the queue, so he can buy again later
+            if tickets[front] != 0:
+                queue.append(front)
+            
+        return time
+
+
+    # Approach-02
+    # TC:O(n*m)=> n is the len of the tickets, m is the maximum no of iteration of outer while loop, SC:O(1)
+    # def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+    #     sold_tickets = 0 # time taken
 
         # keep track of the sold_tickets in each pass untill the tickets[k] == 0
         # while True:
@@ -14,18 +42,19 @@ class Solution:
         #             sold_tickets += 1
 
         # another version of the while loop
-        while tickets[k] > 0:
-            for i in range(len(tickets)):
-                # buy a ticket at idx i if possible; tickets[i] > 0
-                if tickets[i] != 0:
-                    tickets[i] -= 1
-                    sold_tickets += 1
-                # if k bought all it's required tickets, return 
-                if tickets[k] == 0:
-                    return sold_tickets
+        # while tickets[k] > 0:
+        #     for i in range(len(tickets)):
+        #         # buy a ticket at idx i if possible; tickets[i] > 0
+        #         if tickets[i] != 0:
+        #             tickets[i] -= 1
+        #             sold_tickets += 1
+        #         # if k bought all it's required tickets, return 
+        #         if tickets[k] == 0:
+        #             return sold_tickets
 
-        return sold_tickets
+        # return sold_tickets
 
+    # Approach-03
     # TC:O(n), SC:O(1)
     # def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
     #     time = 0
