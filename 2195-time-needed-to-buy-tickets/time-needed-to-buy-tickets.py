9364e49d5731f31ad5a5f219e001a6a238afa116
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-09 12:09:53 +0200

    Time: 55 ms (18.16%) | Memory: 16.5 MB (92.60%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,7 +1,9 @@
 class Solution:
+    # TC:O(n*m)=> n is the len of the tickets, m is the maximum no of iteration of outer while loop, SC:O(1)
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         sold_tickets = 0
 
+        # keep track of the sold_tickets in each pass untill the tickets[k] == 0
         while True:
             for i in range(len(tickets)):
                 tickets[i] -= 1
@@ -10,4 +12,3 @@ class Solution:
                     return sold_tickets
                 if tickets[i] >= 0:
                     sold_tickets += 1
-        
