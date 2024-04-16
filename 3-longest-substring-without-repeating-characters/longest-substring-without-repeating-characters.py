ba150619406100bff174ce751e0287943a5f58c6
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-16 16:06:47 +0200

    Time: 54 ms (64.91%) | Memory: 16.6 MB (96.27%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,19 +1,38 @@
 class Solution:
+    # TC: O(n), SC: O(n)
     def lengthOfLongestSubstring(self, s: str) -> int:
-        max_length, length = 0, 0
+        l, r = 0, 0
+        length, max_length = 0, 0
         freq = dict()
 
-        for i in range(len(s)):
-            freq = dict()
-            length = 0
-            for j in range(i, len(s)):
-                char = s[j]
-                freq[char] = freq.get(char, 0) + 1
-                if freq.get(char, 0) > 1:
-                    break
-                else:
-                    length += 1
-                max_length = max(max_length, length)
+        for r in range(len(s)):
+            char = s[r]
+            freq[char] = freq.get(char, 0) + 1
+
+            while freq.get(char, 0) > 1:
+                freq[s[l]] -= 1
+                l += 1
+            max_length = max(max_length, r-l+1)
 
         return max_length
+
+
+    # TC: O(n^2),  SC: O(n) => Brute Force; AC anyway
+    # def lengthOfLongestSubstring(self, s: str) -> int:
+    #     max_length, length = 0, 0
+    #     freq = dict()
+
+    #     for i in range(len(s)):
+    #         freq = dict()
+    #         length = 0
+    #         for j in range(i, len(s)):
+    #             char = s[j]
+    #             freq[char] = freq.get(char, 0) + 1
+    #             if freq.get(char, 0) > 1:
+    #                 break
+    #             else:
+    #                 length += 1
+    #             max_length = max(max_length, length)
+
+    #     return max_length
 
