2faea8a722b31942ff55a79d9e53e5f33da18cd8
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-24 16:48:50 +0200

    Time: 36 ms (46.62%) | Memory: 16.5 MB (85.24%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,30 @@
+class Solution:
+    # def tribonacci(self, n: int) -> int:
+    #     dp = [-1] * 38
+    #     if n == 0:
+    #         return 0
+    #     if n == 1 or n == 2:
+    #         return 1
+        
+    #     return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
+
+    def createFibonacciOf38(self)-> List:
+        f = [0 , 1]
+
+        for i in range(2, 38):
+            f.append(f[i-1] + f[i-2])
+        
+        return f
+
+    def tribonacci(self, n: int) -> int:
+        f = self.createFibonacciOf38()
+
+        for i in range(len(f)):
+            if i == 0:
+                f[i] = 0
+            elif i == 1 or i == 2:
+                f[i] = 1
+            else:
+                f[i] = f[i-3] + f[i-2] + f[i-1]
+        
+        return f[n]
