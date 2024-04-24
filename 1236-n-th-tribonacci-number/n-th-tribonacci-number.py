748a5558cc8d4cb8f8c32893a457b58e347f2953
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-04-24 17:20:30 +0200

    Time: 32 ms (74.26%) | Memory: 16.5 MB (85.24%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,22 +1,24 @@
 class Solution:
-    def computation(self, n: int, dp: list) -> int:
-        if n in dp:
-            res = dp[n]
-
-        if n == 0:
-            return 0
-        elif n == 1 or n == 2:
-            return 1
-        else:
-            res = computation(n-1) + computation(n-2) + computation(n-3)
-            dp[n] = res
-
-        return res
-
     def tribonacci(self, n: int) -> int:
-        dp = [0]*38
+        dp = {}
 
-        return self.computation(n, dp)
+        def computation(n: int) -> int:
+            if n in dp:
+                res = dp[n]
+                return res
+
+            if n == 0:
+                return 0
+            elif n == 1 or n == 2:
+                return 1
+            else:
+                res = computation(n-1) + computation(n-2) + computation(n-3)
+                dp[n] = res
+
+            return res
+
+        return computation(n)
+
 
     # TLE
     # def tribonacci(self, n: int) -> int:
@@ -28,10 +30,10 @@ class Solution:
     #     return self.tribonacci(n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
 
     # got AC
-    def tribonacci(self, n: int) -> int:
-        f = [0, 1, 1]
+    # def tribonacci(self, n: int) -> int:
+    #     f = [0, 1, 1]
 
-        for i in range(3, 38, 1):
-            f.append(f[i-3] + f[i-2] + f[i-1])
+    #     for i in range(3, 38, 1):
+    #         f.append(f[i-3] + f[i-2] + f[i-1])
         
-        return f[n]
+    #     return f[n]
