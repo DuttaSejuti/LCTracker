de787861aaf6386516e45dc83aed2783023e7986
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-05-02 12:06:59 +0200

    Time: 106 ms (72.32%) | Memory: 16.8 MB (23.86%) - LeetSync

--- 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.py
+++ 2524-largest-positive-integer-that-exists-with-its-negative/largest-positive-integer-that-exists-with-its-negative.py
@@ -1,7 +1,7 @@
 class Solution:
     # store all the elements in a dictionary
     # for all the keys, if the opposite key exists in the dict, calculate the max
-    # TC: O(n), SC:O(n)
+    # TC: O(n), SC:O(n) => 2 pass
     def findMaxK(self, nums: List[int]) -> int:
         new_dict = dict()
         max_ele = -1
@@ -14,3 +14,21 @@ class Solution:
                 max_ele = max(max_ele, abs(k))
         
         return max_ele
+
+# TC:O(n), SC:O(n) => 1 pass
+# class Solution:
+#     def findMaxK(self, nums: List[int]) -> int:
+#         ans = -1
+
+#         # A set to store seen numbers
+#         seen = set()
+
+#         for num in nums:
+#             abs_num = abs(num)
+
+#             # If the absolute value is greater than the current maximum and its negation is seen
+#             if abs_num > ans and -num in seen:
+#                 ans = abs_num
+#             seen.add(num)  # Insert the current number into the set
+
+#         return ans
