64afd1076cffed66f4658d90a85871e7c394ae96
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-05-06 15:56:26 +0200

    Time: 438 ms (44.54%) | Memory: 51.1 MB (78.68%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -3,7 +3,15 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# 1. Reverse the given linked_list, as the last node of the given list will always be present in the result list
+# because, there will be no right node for the last node of the given list.
+# 2. make the first node value of the reversed list as the currMax.
+# 3. create a new LL for the result
+# 4. insert the node in the result_ll if it's value is greater than the currMax, upgrade the currMax
+
 class Solution:
+    # TC: O(n), SC:O(n)
     def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
         curr = head
@@ -26,6 +34,7 @@ class Solution:
             return 
         
         while curr:
+            # get the value that are greater than the previous one's
             if currMax <= curr.val:
                 result_tail.next = ListNode(curr.val)
                 result_tail = result_tail.next
