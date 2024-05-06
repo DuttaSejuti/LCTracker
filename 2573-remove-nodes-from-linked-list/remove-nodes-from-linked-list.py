68453ae67cb8d4b0b38f8397c3bfedd109cdc70d
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-05-06 17:29:23 +0200

    Time: 397 ms (81.02%) | Memory: 51.1 MB (90.29%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -11,7 +11,7 @@
 # 4. insert the node in the result_ll if it's value is greater than the currMax, upgrade the currMax
 
 class Solution:
-    # TC: O(n), SC:O(n)
+    # without extra memory => TC:O(n) SC:O(1)
     def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
         curr = head
@@ -25,23 +25,73 @@ class Solution:
         return prev
 
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        curr = self.reverse(head)
+        head = self.reverse(head)
+        curr = head
         currMax = curr.val
-        result_head = ListNode()
-        result_tail = result_head
 
-        if head is None:
-            return 
+        # because the last node of the given ll is the curr, which will always be present
+        while curr.next:
+            if curr.next.val < currMax:
+                curr.next = curr.next.next # remove curr.next
+            else:
+                currMax = curr.next.val
+                curr = curr.next
+
+        return self.reverse(head)
+
+    # solution with stack => TC:O(n), SC:O(n)
+    # def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     stack = list()
+    #     curr = head
+
+    #     while curr:
+    #         while stack and stack[-1] < curr.val:
+    #             stack.pop()
+    #         stack.append(curr.val)
+    #         curr = curr.next
         
-        while curr:
-            # get the value that are greater than the previous one's
-            if currMax <= curr.val:
-                result_tail.next = ListNode(curr.val)
-                result_tail = result_tail.next
-                currMax = curr.val
-            curr = curr.next
+    #     result_head = ListNode()
+    #     result_tail = result_head
+
+    #     for n in stack:
+    #         result_tail.next = ListNode(n)
+    #         result_tail = result_tail.next
         
-        if result_head.next is None:
-            return head
+    #     return result_head.next
+
+
+# class Solution:
+    # solution without stack but extra memory => TC: O(n), SC:O(n)
+    # def reverse(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     prev = None
+    #     curr = head
+
+    #     while curr:
+    #         next_node = curr.next
+    #         curr.next = prev
+    #         prev = curr
+    #         curr = next_node
         
-        return self.reverse(result_head.next)
+    #     return prev
+
+    # def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    #     curr = self.reverse(head)
+    #     currMax = curr.val
+    #     result_head = ListNode()
+    #     result_tail = result_head
+
+    #     if head is None:
+    #         return 
+        
+    #     while curr:
+    #         # get the value that are greater than the previous one's
+    #         if currMax <= curr.val:
+    #             result_tail.next = ListNode(curr.val)
+    #             result_tail = result_tail.next
+    #             currMax = curr.val
+    #         curr = curr.next
+        
+    #     if result_head.next is None:
+    #         return head
+        
+    #     return self.reverse(result_head.next)
