65869813d7b339fe6f2adb74b52243106270e1b1
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-05-08 16:07:22 +0200

    Time: 70 ms (40.61%) | Memory: 18 MB (22.65%) - LeetSync

--- 506-relative-ranks/relative-ranks.py
+++ 506-relative-ranks/relative-ranks.py
@@ -1,25 +1,49 @@
 class Solution:
+    # without sorting, but TC:O(nlogn), SC:O(n)
     def findRelativeRanks(self, scores: List[int]) -> List[str]:
-        score_idx = dict()
+        heap = []
+        for i, score in enumerate(scores):
+            heapq.heappush(heap, (-score, i))
+        
+        res = ['']*len(scores)
 
-        for i in range(len(scores)):
-            score_idx[scores[i]] = i
-
-        scores.sort(reverse=True)
-        result = ['']*len(scores)
-
-        for i in range(len(scores)):
-            # this uses O(n) to find the idx, use dictionary to access the idx in O(1)
-            # idx = ranked_scores.index(score)
-
-            idx = score_idx[scores[i]]
-            if i == 0:
-                result[idx] = 'Gold Medal'
-            elif i == 1:
-                result[idx] = 'Silver Medal'
-            elif i == 2:
-                result[idx] = 'Bronze Medal'
+        rank = 1
+        while heap:
+            idx = heapq.heappop(heap)[1]
+            if rank == 1:
+                res[idx] = 'Gold Medal'
+            elif rank == 2:
+                res[idx] = 'Silver Medal'
+            elif rank == 3:
+                res[idx] = 'Bronze Medal'
             else:
-                result[idx] = str(i+1)
+                res[idx] = str(rank)
+            rank += 1
+        
+        return res
+
+    # TC:O(nlogn), SC:O(n)
+    # def findRelativeRanks(self, scores: List[int]) -> List[str]:
+    #     score_idx = dict()
+
+    #     for i in range(len(scores)):
+    #         score_idx[scores[i]] = i
+
+    #     scores.sort(reverse=True)
+    #     result = ['']*len(scores)
+
+    #     for i in range(len(scores)):
+    #         # this uses O(n) to find the idx, use dictionary to access the idx in O(1)
+    #         # idx = ranked_scores.index(score)
+
+    #         idx = score_idx[scores[i]]
+    #         if i == 0:
+    #             result[idx] = 'Gold Medal'
+    #         elif i == 1:
+    #             result[idx] = 'Silver Medal'
+    #         elif i == 2:
+    #             result[idx] = 'Bronze Medal'
+    #         else:
+    #             result[idx] = str(i+1)
             
-        return result
+    #     return result
