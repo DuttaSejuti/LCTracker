45063c13e45b4a80e0a8eb9101705dfe4db36728
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-05-09 11:53:41 +0200

    Time: 914 ms (7.65%) | Memory: 43.4 MB (72.04%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -1,18 +1,26 @@
 class Solution:
+    # used max-heap => as only positive value will be present in the array.
+    # TC: O(n + klogn), SC:O(n)
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
         heap = []
         max_sum_happiness = 0
         given_k = k
 
+        # populate the heap as max-heap, max value on top, O(n)
         for h in happiness:
             heapq.heappush(heap, -h)
         
+        # as our target is to pick the largest, the first top value will always be picked at first turn
         max_sum_happiness += - (heapq.heappop(heap))
         k -= 1
 
+        # till we have k and heap, simulate
         while k > 0 and heap:
+            # as we need to decrease the rest of the values, after each turn, rather then decreasing all after each turn
+            # we can simply just consider how many turn has taken place, and decrement it from the popped value
             turn_took = given_k - k
-            top = - (heapq.heappop(heap) + turn_took)
+            top = - (heapq.heappop(heap) + turn_took) # pop takes O(logn), happening k times => O(klogn) 
+
             if top >= 0:
                 max_sum_happiness += top
             k -= 1
