10827d6927e59177c64e7e91141da028dfd7c21f
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-09-26 12:39:33 +0200

    Time: 43 ms (16.04%) | Memory: 16.5 MB (23.97%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,3 +1,7 @@
+# almost similar to the remove duplicates from sorted array algo.
+# one pointer to scan and another pointer to point where we going to insert the element that doesn't match the val.
+    # does match => move r
+    # doesn't match => insert num at pos r to num at pos l, move l, move r
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         l, r = 0 , 0
