aabe63d8787c4bf6681489edcc966996acaeae17
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-09-26 12:37:16 +0200

    Time: 72 ms (74.84%) | Memory: 17.8 MB (79.00%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -4,17 +4,15 @@
 # another pointer to put the next unique value - l
 # compare r to the prev
     # same value => move R
-    # diff value => replace value at l with value at r, move l, move l
+    # diff value => place value at r to value at l, move l, move l
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         l, r = 1, 1
         while r < len(nums):
-            if nums[r] == nums[r-1]:
-                r += 1
-            else:
+            if nums[r] != nums[r-1]:
                 nums[l] = nums[r]
                 l += 1
-                r += 1
+            r += 1
         return l
 
         # shorter version
