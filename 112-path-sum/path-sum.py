23092160d325a80c8e1c062d314290c47ce22cde
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-10-21 20:36:47 +0200

    Time: 4 ms (97.45%) | Memory: 17.6 MB (5.01%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -4,22 +4,42 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+# class Solution:
+#     def dfs(self, root: Optional[TreeNode], target: int, pathSum: int) -> bool:
+#         if not root:
+#             return False
+
+#         pathSum += root.val
+        
+#         if not root.left and not root.right and pathSum == target:
+#             return True
+#         res = False
+#         if root.left:
+#             res |= self.dfs(root.left, target, pathSum)
+#         if root.right:
+#             res |= self.dfs(root.right, target, pathSum)
+
+#         pathSum -= root.val
+#         return res
+
+#     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+#         pathSum = 0
+#         return self.dfs(root, targetSum, pathSum)
+        
 class Solution:
     def dfs(self, root: Optional[TreeNode], target: int, pathSum: int) -> bool:
         if not root:
             return False
-
-        pathSum += root.val
         
-        if not root.left and not root.right and pathSum == target:
+        if not root.left and not root.right and pathSum + root.val == target:
             return True
+
         res = False
         if root.left:
-            res |= self.dfs(root.left, target, pathSum)
+            res |= self.dfs(root.left, target, pathSum + root.val)
         if root.right:
-            res |= self.dfs(root.right, target, pathSum)
+            res |= self.dfs(root.right, target, pathSum + root.val)
 
-        pathSum -= root.val
         return res
 
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
