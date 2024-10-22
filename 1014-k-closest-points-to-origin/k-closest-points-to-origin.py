218d2d8a442b9fd83c1cae336134d434e8e22ebe
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-10-22 12:25:01 +0200

    Time: 85 ms (96.19%) | Memory: 21.8 MB (96.88%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -0,0 +1,22 @@
+class Solution:
+    def calculateDistance(self, point: List[int]):
+        x1, y1 = 0, 0 # origin
+        x2, y2 = point[0], point[1]
+
+        eu_distance = math.sqrt(pow((x1-x2), 2) + pow((y1-y2), 2))
+
+        return eu_distance
+
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        result = []
+        for point in points:
+            distance = self.calculateDistance(point)
+            heapq.heappush(minHeap, (distance, point)) # tuple => (distance, [co-ordinate])
+        
+        while k:
+            res = heapq.heappop(minHeap)
+            result.append(res[1]) # append the co-ordinate
+            k -= 1
+        
+        return result
