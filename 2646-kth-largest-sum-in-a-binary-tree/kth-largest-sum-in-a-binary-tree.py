033ef7e8fea4fea01989d8db6b1538f5dd2a4089
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-10-22 11:12:40 +0200

    Time: 59 ms (99.22%) | Memory: 53 MB (15.99%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
@@ -4,15 +4,38 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def kthLargest(self, sumList: List[int], k: int) -> int:
-        if len(sumList) < k: return -1
-        sumList.sort(reverse=True)
-        return sumList[k-1]
+# class Solution:
+#     def kthLargest(self, sumList: List[int], k: int) -> int:
+#         if len(sumList) < k: return -1
+#         sumList.sort(reverse=True)
+#         return sumList[k-1]
 
+#     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
+#         queue = deque()
+#         sum_list = []
+
+#         if not root:
+#             return 0
+        
+#         queue.append(root)
+#         while len(queue) > 0:
+#             level_sum = 0
+#             for i in range(len(queue)):
+#                 curr = queue.popleft()
+#                 level_sum += curr.val
+#                 if curr.left: queue.append(curr.left)
+#                 if curr.right: queue.append(curr.right)
+#             sum_list.append(level_sum)
+        
+#         return self.kthLargest(sum_list, k)
+
+
+# TC: O(logn+logk)
+# SC: O(n)
+class Solution:
     def kthLargestLevelSum(self, root: Optional[TreeNode], k: int) -> int:
         queue = deque()
-        sum_list = []
+        minHeap = []
 
         if not root:
             return 0
@@ -23,10 +46,13 @@ class Solution:
             for i in range(len(queue)):
                 curr = queue.popleft()
                 level_sum += curr.val
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-            sum_list.append(level_sum)
+                if curr.left: queue.append(curr.left)
+                if curr.right: queue.append(curr.right)
+            heapq.heappush(minHeap, level_sum)
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
         
-        return self.kthLargest(sum_list, k)
+        if len(minHeap) < k:
+            return -1
+
+        return minHeap[0]
