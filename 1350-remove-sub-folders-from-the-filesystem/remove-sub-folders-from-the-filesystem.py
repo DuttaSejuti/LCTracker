bd1bae6a22bb00b3315b50ff91832ad509ddada0
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-10-26 02:02:19 +0200

    Time: 45 ms (86.46%) | Memory: 30.2 MB (75.04%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,31 +1,30 @@
 class TrieNode:
     def __init__(self):
         self.children = {}
-        self.word = False
+        self.end_of_folder = False
+    
+    def insert(self, path: str) -> bool:
+        curr = self
+        # as the string starts with /, this will create an empty string for the first /, hence string[1:]
+        folder = path[1:].split('/')
+        for c in folder:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+            if curr.end_of_folder:
+                return False
+        curr.end_of_folder = True
+        return True
 
 class Solution:
-    def __init__(self):
-        self.root = TrieNode()
-
     def removeSubfolders(self, folder: List[str]) -> List[str]:
+        trie = TrieNode()
         sorted_folders = sorted(folder, key = len)
-        root = TrieNode()
         result = []
 
         for i in range(len(sorted_folders)):
-            folder = sorted_folders[i].split('/')
-            flag = 0
-            curr = root
-            for c in folder:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-                if curr.word:
-                    flag = 1
-                    break
-            if flag == 0:
-                curr.word = True
-                result.append(sorted_folders[i])
-
+            folder = sorted_folders[i]
+            insert_possible = trie.insert(folder) 
+            if insert_possible:
+                result.append(folder)
         return result
-                   
