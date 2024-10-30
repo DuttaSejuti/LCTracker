6d374eff6ce72afe5f0f9e3b73a01d87a288c828
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-10-30 13:47:42 +0100

    Time: 0 ms (100.00%) | Memory: 29.1 MB (95.78%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -1,10 +1,24 @@
+# Linear Solution O(n) TC
+# class Solution:
+#     def peakIndexInMountainArray(self, arr: List[int]) -> int:
+#         peak_index = 0
+
+#         for i in range(len(arr)-1):
+#             if arr[i] > arr[i+1]:
+#                 peak_index = i
+#                 break
+        
+#         return peak_index
+
+# Binary Search O(nlogn)
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        peak_index = 0
+        l, r = 0, len(arr)-1
 
-        for i in range(len(arr)-1):
-            if arr[i] > arr[i+1]:
-                peak_index = i
-                break
-        
-        return peak_index
+        while l < r:
+            m = (l+r) // 2
+            if arr[m] < arr[m+1]: # we are still in the increasing phase, peak idx is in right
+                l = m+1
+            else:
+                r = m # we are in decreasing phase, peak is either mid or some idx smaller than mid, go left
+        return l
