256f246e7bf451a9aca9ff4064bb53f9a2a9e13b
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-11-01 12:48:08 +0100

    Time: 154 ms (100.00%) | Memory: 18.5 MB (73.10%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,27 +1,33 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
         result = []
-        if len(s) <=2 : return s
-        
-        for i in range(len(s)-2):
-            a, b, c = s[i], s[i+1], s[i+2]
+        count = 0
+        c = '#' # for checking the first char
 
-            if a != b or b != c or a != c:
-                result.append(a)
-
-            if i == len(s) - 3:
-                result.append(b)
-                result.append(c)
-
-            # if a == b == c:
-            #     if i == len(s) - 3:
-            #         result.append(b)
-            #         result.append(c)
-            #     continue
-            # else:
-            #     result.append(a)
-            #     if i == len(s) - 3:
-            #         result.append(b)
-            #         result.append(c)
+        for char in s:
+            if char == c:
+                count += 1
+            else:
+                c = char
+                count = 1
+            if count < 3:
+                result.append(char)
 
         return ''.join(result)
+
+# class Solution:
+#     def makeFancyString(self, s: str) -> str:
+#         result = []
+#         if len(s) <=2 : return s
+
+#         for i in range(len(s)-2):
+#             a, b, c = s[i], s[i+1], s[i+2]
+
+#             if a != b or b != c or a != c:
+#                 result.append(a)
+
+#             if i == len(s) - 3:
+#                 result.append(b)
+#                 result.append(c)
+
+#         return ''.join(result)
