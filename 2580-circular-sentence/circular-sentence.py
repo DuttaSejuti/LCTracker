72606437d7ab4ff060b2e681bc8e1ceaa1638e6b
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-11-02 12:47:16 +0100

    Time: 0 ms (100.00%) | Memory: 16.6 MB (45.68%) - LeetSync

--- 2580-circular-sentence/circular-sentence.py
+++ 2580-circular-sentence/circular-sentence.py
@@ -1,3 +1,4 @@
+# TC: O(n). SC: O(n)
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
         words = sentence.split(' ')
@@ -7,4 +8,12 @@ class Solution:
                 return False
             prev = words[i][-1]
         
-        return True if prev == words[0][0] else False 
+        return True if prev == words[0][0] else False # check for the last word and the first one
+
+# TC: O(n), SC: O(1)
+# class Solution:
+#     def isCircularSentence(self, sentence: str) -> bool:
+#         for i in range(len(sentence)):
+#             if sentence[i] == ' ' and sentence[i - 1] != sentence[i + 1]:
+#                 return False
+#         return sentence[0] == sentence[len(sentence) - 1]
