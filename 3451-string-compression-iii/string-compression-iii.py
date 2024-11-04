a60b5df852006d4e4ec6784be4f6f15c0b599f95
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-11-04 12:08:00 +0100

    Time: 277 ms (37.59%) | Memory: 27.3 MB (17.62%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,21 +1,39 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-        result = []
-        count = 1
+        i = 0
+        result  = []
 
-        for i in range(len(word)-1):
-            if word[i] == word[i+1]:
+        while i < len(word):
+            count = 1 # Start counting the current character
+            curr = word[i]
+
+             # Count up to 9 consecutive characters
+            while i + 1 < len(word) and word[i+1] == curr and count < 9:
                 count += 1
-                if count > 9:
-                    result.append(str(count-1))
-                    result.append(word[i])
-                    count = 1
-            else:
-                result.append(str(count))
-                result.append(word[i])  
-                count = 1
-        result.append(str(count))
-        result.append(word[-1])
+                i += 1
             
-        return ''.join(result)
+            result.append(str(count) + curr) 
+            i += 1 # Move to the next character group
 
+        return ''.join(result)
+    
+# class Solution:
+#     def compressedString(self, word: str) -> str:
+#         result = []
+#         count = 1
+
+#         for i in range(len(word)-1):
+#             if word[i] == word[i+1]:
+#                 count += 1
+#                 if count > 9:
+#                     result.append(str(count-1))
+#                     result.append(word[i])
+#                     count = 1
+#             else:
+#                 result.append(str(count))
+#                 result.append(word[i])  
+#                 count = 1
+#         result.append(str(count))
+#         result.append(word[-1])
+            
+#         return ''.join(result)
