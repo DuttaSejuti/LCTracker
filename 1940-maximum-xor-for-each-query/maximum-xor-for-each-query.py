ea456066bfd40888fd7b42ca30cc166b9f0beb8b
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-11-08 16:36:53 +0100

    Time: 680 ms (6.25%) | Memory: 29.7 MB (97.67%) - LeetSync

--- 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
+++ 1940-maximum-xor-for-each-query/maximum-xor-for-each-query.py
@@ -6,59 +6,53 @@
 
 # a ^ b ^ c = total_xor
 # total_xor ^ c = a ^ b
-class Solution:    
+
+# solution with assuming the max_xor will be (1<<maximumbit)-1
+# class Solution:    
+#     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
+#         result = []
+#         length = len(nums)
+
+#         xor_nums = 0
+#         for n in nums:
+#             xor_nums ^= n  
+
+#         # it is guranteed that the maximized xor will be 2^(maximumBit) - 1
+#         # because of "0 <= nums[i] < 2^maximumBit" this constraint
+#         # creates a binary number with exactly k bits sets to '1'
+#         max_possible_xor = (1 << maximumBit) - 1
+
+#         for _ in range(length):
+#             max_k = xor_nums ^ max_possible_xor
+#             result.append(max_k)
+#             last_val = nums.pop()
+#             # update xor_nums by performing xor with the popped element and the previous xor
+#             xor_nums = xor_nums ^ last_val
+
+#         return result
+
+class Solution:
+    def getComplementXOR(self, xor_nums: int, maximumBit: int) -> int:
+        max_k = 0
+        for bit in range(maximumBit):
+            mask = 1 << bit
+            # check if the bit at position "bit" in xor_nums is 0; if so, set that bit in max_k
+            if xor_nums & mask == 0:
+                max_k |= mask
+        return max_k
+
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
         result = []
         length = len(nums)
 
         xor_nums = 0
         for n in nums:
-            xor_nums ^= n  
-
-        # it is guranteed that the maximized xor will be 2^(maximumBit) - 1
-        # because of "0 <= nums[i] < 2^maximumBit" this constraint
-        # creates a binary number with exactly k bits sets to '1'
-        max_possible_xor = (1 << maximumBit) - 1
-
+            xor_nums ^= n
+        
         for _ in range(length):
-            max_k = xor_nums ^ max_possible_xor
+            max_k = self.getComplementXOR(xor_nums, maximumBit) # basically flipped_xor
             result.append(max_k)
             last_val = nums.pop()
-            # update xor_nums by performing xor with the popped element and the previous xor
             xor_nums = xor_nums ^ last_val
-
+        
         return result
-
-
-# TC: O(n*2^maximumBit + n^2) => TLE
-# class Solution:
-#     def computeListXOR(self, nums: List[int]) -> int:
-#         ans = 0
-#         for n in nums:
-#             ans ^= n
-#         return ans
-    
-#     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-#         result = []
-#         k_list = []
-#         max_xor = 0
-        
-#         # constructing k_list to store the all possible values of k (1 to 2^maxbit)
-#         for k in range(0, pow(2, maximumBit)):
-#             k_list.append(k)
-        
-#         while len(nums) > 0:
-#             xor_num = self.computeListXOR(nums)
-#             for k in k_list:
-#                 # if the xor of nums is 0, the max_k will maximize the xor, no need to compute max_xor
-#                 if xor_num == 0:
-#                     max_k = k_list[-1]
-#                     break
-#                 max_xor = max(max_xor, xor_num ^ k)
-#                 if max_xor == xor_num ^ k:
-#                     max_k = k
-#             result.append(max_k)
-#             max_xor = 0
-#             nums.pop()
-
-#         return result
