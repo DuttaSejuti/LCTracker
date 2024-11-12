b145532997cd5d1267b15b2592bcf88a4cce07cc
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-11-12 19:35:31 +0100

    Time: 0 ms (100.00%) | Memory: 16.8 MB (20.62%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.py
+++ 191-number-of-1-bits/number-of-1-bits.py
@@ -1,8 +1,30 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
-        for bit in range(32):
-            bit_mask = 1 << bit
-            if n & bit_mask > 0:
+        while n:
+            if n & 1 > 0:
                 count += 1
+            n >>= 1 # right shift n by 1 bit
         return count
+
+# O(1)
+# class Solution:
+#     def hammingWeight(self, n: int) -> int:
+#         count = 0
+#         for bit in range(32):
+#             bit_mask = 1 << bit
+#             if n & bit_mask > 0:
+#                 count += 1
+#         return count
+
+
+# Brian Kernighan's algorithm
+# class Solution:
+#     def hammingWeight(self, num: int) -> int:
+#         count = 0
+#         while num:
+#             num &= (num-1)
+#             count += 1
+#         return count
+
+
