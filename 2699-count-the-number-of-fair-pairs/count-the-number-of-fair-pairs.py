668ffec5b9ce77725187f941db4903378cef4eb7
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-11-13 18:40:53 +0100

    Time: 126 ms (73.21%) | Memory: 30.7 MB (71.63%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.py
@@ -0,0 +1,29 @@
+# 0, 1, 4, 4, 5, 7
+class Solution:
+    def findPairsLessThanLower(self, nums: List[int], lower) -> int:
+        l, r = 0, len(nums)-1
+        count = 0
+
+        while l < r:
+            while l < r and r < len(nums) and nums[l] + nums[r] < lower:
+                count += (r-l)
+                l += 1
+            r -= 1
+        return count
+    
+    def findPairsLessThanOrEqualUpper(self, nums: List[int], upper) -> int:
+        l, r = 0, len(nums)-1
+        count = 0
+
+        while l < r:
+            while l < r and r < len(nums) and nums[l] + nums[r] <= upper:
+                count += (r-l)
+                l += 1
+            r -= 1
+        return count
+
+    def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        nums.sort()
+        pairs_less_than_lower = self.findPairsLessThanLower(nums, lower)
+        pairs_less_than_equal_to_upper = self.findPairsLessThanOrEqualUpper(nums, upper)
+        return  pairs_less_than_equal_to_upper - pairs_less_than_lower
