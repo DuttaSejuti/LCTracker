2f1187eb2b77db0d6ffe9b115d0ee5c5767646b0
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-12-02 15:04:07 +0100

    Time: 0 ms (100.00%) | Memory: 17.1 MB (5.67%) - LeetSync

--- 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
+++ 1566-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.py
@@ -1,14 +1,33 @@
+# TC: O(n*k); SC:O(1)
 class Solution:
     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
-        words = sentence.split(' ')
-
-        for i in range(len(words)):
-            word = words[i]
-            for j in range(len(word)):
-                if word[j] == searchWord[j]:
-                    if j == len(searchWord) - 1:
-                        return i + 1
-                else:
-                    break
+        wordPos, idx = 1, 0
+        
+        while idx < len(sentence):
+            if sentence[idx: idx + len(searchWord)] == searchWord:
+                # boundary check, if the substring is the start of the word
+                # if i == 0, do not have to check for space, that's why or
+                if idx == 0 or sentence[idx-1] == ' ':
+                    return wordPos
+            if sentence[idx] == ' ':
+                wordPos += 1
+            idx += 1
 
         return -1
+            
+
+# TC: O(n*m); SC:O(n)
+# class Solution:
+#     def isPrefixOfWord(self, sentence: str, searchWord: str) -> int:
+#         words = sentence.split(' ')
+
+#         for i in range(len(words)):
+#             word = words[i]
+#             for j in range(len(word)):
+#                 if word[j] == searchWord[j]:
+#                     if j == len(searchWord) - 1:
+#                         return i + 1
+#                 else:
+#                     break
+
+#         return -1
