a8801f3920f801d699d5e8b111dbdf0989b9aad8
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2024-12-03 08:37:53 +0100

    Time: 43 ms (86.30%) | Memory: 49.1 MB (36.87%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.py
@@ -1,15 +1,30 @@
+# O(n) => Optimized
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
-        new_str = ''
-        k = 0
+        result = []
         start = 0
+        
+        for space in spaces:
+            result.append(s[start:space])
+            result.append(' ')
+            start = space
+        
+        result.append(s[start:])
+        return ''.join(result)
 
-        while k < len(spaces): # O(k)
-            end = spaces[k]
-            for i in range(start, end, 1): # O(m)
-                new_str += s[i]
-            new_str += ' '
-            start = end # new start will be the ending index
-            k += 1
+# O(n^2) => Brute Force
+# class Solution:
+#     def addSpaces(self, s: str, spaces: List[int]) -> str:
+#         new_str = ''
+#         k = 0
+#         start = 0
 
-        return new_str + s[end:]
+#         while k < len(spaces): # O(m)
+#             end = spaces[k]
+#             for i in range(start, end, 1): # O(n)
+#                 new_str += s[i] # this one creates a new string; O(k)
+#             new_str += ' '
+#             start = end # new start will be the ending index
+#             k += 1
+
+#         return new_str + s[end:]
