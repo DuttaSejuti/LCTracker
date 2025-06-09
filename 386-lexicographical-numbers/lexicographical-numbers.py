df37bccb41c0b63f6dad2a5476387306e92745ae
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2025-06-10 00:50:33 +0200

    Time: 11 ms (70.67%) | Memory: 21.3 MB (56.05%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,5 +1,22 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        numbers = list(range(1, n+1))
-        numbers.sort(key=str)
-        return numbers
+        result = list()
+        curr = 1
+
+        #as we want to generate n numbers
+        for _ in range(n):
+            result.append(curr)
+            #go deeper
+            if curr*10 <= n:
+                curr *= 10
+            #go to next childen
+            elif curr % 10 != 9 and curr + 1 <=n:
+                curr += 1
+            else:
+                #find next root node
+                while curr % 10 == 9 or curr + 1 > n:
+                    curr //= 10
+                curr += 1
+        return result
+
+
