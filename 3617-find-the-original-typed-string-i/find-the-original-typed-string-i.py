769f80b743b104a9c9b4da95024590d391f7a3e9
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2025-07-18 16:57:32 +0200

    Time: 46 ms (15.86%) | Memory: 17.8 MB (31.45%) - LeetSync

--- 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
+++ 3617-find-the-original-typed-string-i/find-the-original-typed-string-i.py
@@ -3,6 +3,7 @@ class Solution:
         new_dict = dict()
         prev = word[0]
         for i in range(1, len(word)):
+            print("i", i)
             if prev == word[i]: # keeping track of the consequtive char
                 if new_dict.get(word[i], 0) == 0:
                     new_dict[word[i]] = 2
@@ -10,10 +11,6 @@ class Solution:
                     new_dict[word[i]] = new_dict.get(word[i], 0) + 1   
             prev = word[i]
         
-        # if prev == word[-1]:
-        #     new_dict[word[-1]] = new_dict.get(word[-1], 0) + 1
-        
-        print(new_dict)
         total_count = 1 # the original string itself can be an intended string
         for k in new_dict.keys():
             if new_dict[k] > 1:
