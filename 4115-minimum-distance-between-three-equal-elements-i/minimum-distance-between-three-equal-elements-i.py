5239d3c6d05ed2f730d454814fdf35978ebfdf22
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2026-04-10 16:53:55 +0200

    Time: 0 ms (100.00%) | Memory: 19.1 MB (95.64%) - LeetSync

--- 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
+++ 4115-minimum-distance-between-three-equal-elements-i/minimum-distance-between-three-equal-elements-i.py
@@ -1,4 +1,5 @@
 class Solution:
+    # TC: O(n) SC: O(n)
     def minimumDistance(self, nums: List[int]) -> int:
         freq_map = defaultdict(list)
         min_distance = float('inf')
@@ -10,17 +11,12 @@ class Solution:
             if len(v) >= 3:
                 for i in range(0, len(v) - 2, 1):
                     # no abs(), as the indices are appended in the dict in sorted manner
-                    temp_distance = (v[i+1]-v[i]) + (v[i+2]-v[i+1]) + (v[i+2]-v[i])
+                    # temp_distance = (v[i+1]-v[i]) + (v[i+2]-v[i+1]) + (v[i+2]-v[i])
+
+                    # 2 * (max_index - min_index)  {2*(k-i)} for any three indices
+                    temp_distance = 2 * (v[i+2] - v[i])
                     min_distance = min(temp_distance, min_distance)
 
         if min_distance == float('inf'): return -1
 
         return min_distance
-
-    # def minimumDistance(self, nums: List[int]) -> int:
-    #     freq_map = dict()
-    #     min_distance = float('inf')
-
-    #     for i, num in enumerate(nums):
-    #         freq_map[num].append(i)
-        
