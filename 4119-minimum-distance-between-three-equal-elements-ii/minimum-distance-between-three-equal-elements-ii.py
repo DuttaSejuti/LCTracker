73bc1799ae024fce3a2296860bc65d79b5dfaf89
Sejuti <43231956+DuttaSejuti@users.noreply.github.com>
2026-04-11 15:33:35 +0200

    Time: 302 ms (81.82%) | Memory: 50.4 MB (70.59%) - LeetSync

---
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumDistance(self, nums: List[int]) -> int:
+        freq_map = defaultdict(list)
+        min_distance = float('inf')
+
+        for i, num in enumerate(nums):
+            freq_map[num].append(i)
+        
+        for k, v in freq_map.items():
+            if len(v) >= 3:
+                for i in range(0, len(v) - 2, 1):
+                    temp_distance = 2 * (v[i+2] - v[i])
+                    min_distance = min(temp_distance, min_distance)
+
+        if min_distance == float('inf'): return -1
+
+        return min_distance
